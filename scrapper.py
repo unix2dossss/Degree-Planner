3312c8a03bcbae3b4f11a4c4d25d3b045d2f53b8
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-06-30 18:45:58 +1200

    Scraper Finds Course Codes

--- scrapper.py
+++ scrapper.py
@@ -12,13 +12,16 @@ doc = BeautifulSoup(result.text, "html.parser")
 divs = doc.find_all('div', class_='courseStage section')
 
 for stage in divs:
-    # Finds Stage Number/Title.
+    # Finds Stage Number/Title. | Example: Stage I
     stage_part = stage.find('h3', class_='stage').text.strip()
     data[stage_part] = dict()
+    # Finds Course Code Number | Example: COMPSCI 101
+    courses_div = stage.find_all('div', class_='coursePaper section')
+    for course in courses_div:
+        course_code = course.find('div', class_='courseA').text.strip()
+        data[stage_part][course_code] = dict()
 
 print(data)
-
-# print(data)
 
 # with open("course_data.json", "w" ) as file:
 #     pass
