b72e0775385fdece2f885616abfd4144035089e5
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-06-30 18:33:11 +1200

    Scraper Finds Stages

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+data = dict()
+
 url = "https://www.calendar.auckland.ac.nz/en/courses/faculty-of-science/computer-science.html"
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
@@ -9,4 +11,14 @@ doc = BeautifulSoup(result.text, "html.parser")
 
 divs = doc.find_all('div', class_='courseStage section')
 
-print(len(divs))
+for stage in divs:
+    # Finds Stage Number/Title.
+    stage_part = stage.find('h3', class_='stage').text.strip()
+    data[stage_part] = dict()
+
+print(data)
+
+# print(data)
+
+# with open("course_data.json", "w" ) as file:
+#     pass
