3919e3bea3f43de1bba300d53315d9afc3587fee
Vandan Bhatt <60042992+unix2dossss@users.noreply.github.com>
2023-07-01 04:16:32 +1200

    Cleaned data for Restrictions

--- datacleaner.py
+++ datacleaner.py
@@ -1,3 +1,7 @@
+"""
+Add few restrictions in Stage I manually into output json.
+"""
+
 import json
 import re
 
@@ -19,13 +23,48 @@ def parse_course_string(course_string):
 
     return parsed_courses
 
+all_courses = []
+
+for stage in data.keys():
+    for course in data[stage]:
+        all_courses.append(course)
+
+def course_restriction_range_finder(restriction_string):
+    cannot_take_courses = []
+    # Extract individual courses
+    courses = re.findall(r'COMPSCI \d+', restriction_string)
+    cannot_take_courses.extend(courses)
+
+    # Extract course ranges
+    ranges = re.findall(r'(\d+)-(\d+)', restriction_string)
+    
+    for start, end in ranges:
+        start = int(start)
+        end = int(end)
+        for course in all_courses:
+            course_number = re.findall(r'\d+', course)[0]  # Extract numeric course number
+            if start <= int(course_number) <= end:
+                cannot_take_courses.append(course)
+    
+    return cannot_take_courses
+
+
 for stage in data.keys():
     print('---')
     print(stage)
     print('---')
     for course in data[stage]:
         if 'Restriction' in data[stage][course].keys():
-            print(parse_course_string(data[stage][course]['Restriction']))
+            if 'Cannot be taken with' in data[stage][course]['Restriction']:
+                # print(data[stage][course]['Restriction'])
+                rs = course_restriction_range_finder(data[stage][course]['Restriction'])
+                data[stage][course]['Take Before'] = rs
+            else:
+                data[stage][course]['Restriction'] = parse_course_string(data[stage][course]['Restriction'])
+
+
+json_str = json.dumps(data, indent=4)
+print(json_str)
 
 '''
 Catergories
@@ -34,5 +73,4 @@ Catergories
 
 Filters Required
 - Cannot be taken with
-
 '''
